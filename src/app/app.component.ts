156b4eb761151c83d9d6c1f2ccbc4f7f09a03bfc
zoudoum <doumzoro@gmail.com>
2023-11-15 14:50:51 +0000

    creation du grille avec html css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jeuDeLaVieJohnConway';
+  gridColumns = 30;
+  gridRows = 20;
+  grid: boolean[][] = [];
+
+  constructor() {
+    for (let i = 0; i < this.gridRows; i++) {
+      const row = [];
+      for (let j = 0; j < this.gridColumns; j++) {
+        row.push(false);
+      }
+      this.grid.push(row);
+    }
+    
+  }
+
+
 }
