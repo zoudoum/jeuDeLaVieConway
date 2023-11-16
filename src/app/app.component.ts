1923b308b26f4f9c82b9028425af57d1db4144c8
zoudoum <doumzoro@gmail.com>
2023-11-16 00:52:44 +0000

    ajouter des fonctions pour compter le nombre de cellules voisines vivantes et l'implémentation de l'algorithme suivant les regles du jeu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jeuDeLaVieJohnConway';
-  gridColumns = 30;
-  gridRows = 20;
-  grid: boolean[][] = [];
+  gridColumns=30;
+  gridRows=20;
+  grid: boolean[][]=[];
+  jouer:boolean=true;
+  private intervalId: any;
+  stop:boolean=false
 
-
-  ngOnInit(): void {
+ngOnInit(): void {
     this.initialiserGrid();
     this.initialiserGridActive()
-   
   }
 
-  initialiserGrid(){
+initialiserGrid(){
     for (let i = 0; i < this.gridRows; i++) {
       const row = [];
       for (let j = 0; j < this.gridColumns; j++) {
@@ -26,21 +27,162 @@ export class AppComponent {
       }
       this.grid.push(row);
     }
-
   }
-   initialiserGridActive(){
+
+initialiserGridActive(){
   this.grid[1][2] = true;
   this.grid[2][3] = true;
   this.grid[3][1] = true;
   this.grid[3][2] = true;
   this.grid[3][3] = true;
 }
+
 onClickCell(rowIndex: number, colIndex: number) {
   if( this.grid[rowIndex][colIndex]==false)
   this.grid[rowIndex][colIndex]=true;
   else{
     this.grid[rowIndex][colIndex]=false;
   }
+}
+
+clearGrid() {
+  this.grid = [];
+  for (let row = 0; row < this.gridRows; row++) {
+    this.grid[row] = [];
+    for (let col = 0; col < this.gridColumns; col++) {
+      this.grid[row][col] = false; 
+    }
+  }
+}
+
+private startGame() {
+  this.intervalId = setInterval(() => {  
+      this.updateGrid();
+  }, 2000);
+}
+
+private stopGame() {
+  if (this.intervalId) {
+    clearInterval(this.intervalId);
+  }
+}
+
+private updateGrid() {
+  const newGrid: boolean[][] = [];
+  for (let i = 0; i < this.grid.length; i++) {
+    const newRow = [];
+    for (let j = 0; j < this.grid[i].length; j++) {  
+      const voisin = this.countVoisinEnVie(i, j); 
+      if (this.grid[i][j]) {   
+        if (voisin <= 1 || voisin >= 4) {
+                newRow.push(false);
+        } else {
+          
+                 newRow.push(true);     
+        }
+      } else {
+      
+        if (voisin===3) {
+                 
+          newRow.push(true);
+          
+        } else {
+          
+          newRow.push(false);
+        }
+      }
+    }
+    newGrid.push(newRow);
+  }
+  this.grid = newGrid;
+}
+private countVoisinEnVie(x: number, y: number): number {
+    
+  let count = 0;
+  
+if (this.gridRows - 1 > x && y < this.gridColumns - 1) { 
+  if(x + 1 < this.gridRows&&y + 1 < this.gridColumns ){
+    if(this.grid[x+1][y+1]){
+        count++
+    }
+ }
+}
+
+if(x>0&&y>0){
+  if(this.grid[x-1][y-1]){
+     count++
+  }
+}
+
+if (y < this.gridColumns - 1) {
+if(y+1<this.gridColumns){
+  if(this.grid[x][y+1]){
+     count++
+  }
+}
+}
+
+
+
+if(x>0){
+  if(this.grid[x-1][y]){
+     count++
+  }
+}
+
+
+if(y>0){
+  if(this.grid[x][y-1]){
+     count++
+  }
+}
+
+if (this.gridRows - 1 > x) {
+if(y+1<this.gridColumns){
+  if(this.grid[x+1][y]){
+     count++
+  }
+}
+}
+
+if (this.gridRows - 1 > x) {
+ if(y>0&&y+1<this.gridColumns){
+   if(this.grid[x+1][y-1]){
+      count++
+   }
+ }
+}
+
+if ( y < this.gridColumns - 1) {
+ if(y+1<=this.gridColumns&&x>0){
+   if(this.grid[x-1][y+1]){
+      count++
+   }
+ }
+}
+
+return count;
+ 
+}
+
+
+onClickStart() {
+  this.jouer=false;
+  this.stop=true;
+  this.startGame();
+}
+
+onClickRestart() {
+  this.jouer=true
+  this.stop=false;
+  this.stopGame();
+  this.clearGrid();
+}
+
+onClickStop(){
+  this.jouer=true
+  this.stop=false
+this.stopGame();
 }
 
 }
